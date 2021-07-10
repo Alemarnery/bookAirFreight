c14597d95338b2fcef4fd8820f09d1c053e9aef9
Alemarnery <aleemar.95@gmail.com>
2021-07-09 23:35:05 -0500

    start and end day

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <Form />
-      <div>
-        <h1>Form Result</h1>
-      </div>
     </div>
   );
 }
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 const Form = () => {
   const [inputs, setInputs] = useState(null);
+  const [submit, setSubmit] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
+    setSubmit(true);
   };
 
   const onChanged = (e) => {
@@ -15,6 +17,75 @@ const Form = () => {
       [name]: value,
     });
   };
+
+  const RenderedTable = () => {
+    if (submit) {
+      const { shipping, startCountry, destinationCountry, quotePrice } = inputs;
+
+      let START_MIN_DAY;
+      let START_MAX_DAY;
+
+      let END_MIN_DAY;
+      let END_MAX_DAY;
+
+      switch (shipping) {
+        case "Air":
+          START_MIN_DAY = 3;
+          START_MAX_DAY = 8;
+
+          END_MIN_DAY = 2;
+          END_MAX_DAY = 5;
+
+          break;
+        case "Ocean":
+          START_MIN_DAY = 25;
+          START_MAX_DAY = 31;
+
+          END_MIN_DAY = 5;
+          END_MAX_DAY = 11;
+          break;
+        default:
+          break;
+      }
+
+      // Retorna un entero aleatorio entre min (incluido) y max (excluido)
+      const startDay = Math.floor(
+        Math.random() * (START_MAX_DAY - START_MIN_DAY) + START_MIN_DAY
+      );
+
+      const endDay =
+        startDay +
+        Math.floor(Math.random() * (END_MAX_DAY - END_MIN_DAY) + END_MIN_DAY);
+
+      console.log(new Date());
+
+      return (
+        <table>
+          <tr>
+            <th>Traditional {shipping} freight</th>
+            <th>
+              {startCountry} - {destinationCountry}
+            </th>
+          </tr>
+          <tbody>
+            <tr>
+              <td>
+                {startDay} - {endDay} days
+              </td>
+              <td>US $</td>
+            </tr>
+            <tr>
+              <td>Estimated delivery</td>
+              <td>info {quotePrice}</td>
+            </tr>
+          </tbody>
+        </table>
+      );
+    }
+    return null;
+  };
+
+  console.log(inputs);
 
   return (
     <div>
@@ -41,7 +112,11 @@ const Form = () => {
         <div>
           <button type="submit">Submit</button>
         </div>
-        yvuvyv jujuju yyhyhy8
+
+        <div>
+          <h1>Resume</h1>
+          <RenderedTable />
+        </div>
       </form>
     </div>
   );
