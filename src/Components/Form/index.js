0955581248e40ee40b6ca2c6a4720baffa257a77
Alemarnery <aleemar.95@gmail.com>
2021-07-10 01:38:59 -0500

    days

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -6,7 +6,6 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputs);
     setSubmit(true);
   };
 
@@ -49,33 +48,66 @@ const Form = () => {
       }
 
       // Retorna un entero aleatorio entre min (incluido) y max (excluido)
+      //   const startDay = Math.floor(
+      //     Math.random() * (START_MAX_DAY - START_MIN_DAY) + START_MIN_DAY
+      //   );
+
       const startDay = Math.floor(
         Math.random() * (START_MAX_DAY - START_MIN_DAY) + START_MIN_DAY
       );
 
-      const endDay =
-        startDay +
-        Math.floor(Math.random() * (END_MAX_DAY - END_MIN_DAY) + END_MIN_DAY);
+      const waitDay = Math.floor(
+        Math.random() * (END_MAX_DAY - END_MIN_DAY) + END_MIN_DAY
+      );
 
-      console.log(new Date());
+      const endDay = startDay + waitDay;
+
+      console.log("startDay", startDay);
+      console.log("dias de espera", waitDay);
+      console.log("endDay", endDay);
+
+      /**CREAR UNA FUNCION*/
+      //   const date = new Date();
+      //   let monthName = date.toLocaleString("default", { month: "long" });
+
+      //   let month = date.getMonth() + 1;
+      //   const year = date.getFullYear();
+      //   const days = new Date(year, month, 0).getDate(); //dias del Mes
+      //   const todayNumber = date.getDate(); // dia del mes
+      //   let deliveryStart = todayNumber + startDay;
+      //   let deliveryEnd = todayNumber + endDay;
+
+      //   if (deliveryStart > days) {
+      //     deliveryStart = deliveryStart - days;
+      //     month++;
+      //     monthName = new Date(year, month, 0).toLocaleString("default", {
+      //       month: "long",
+      //     });
+      //     deliveryEnd = deliveryStart + waitDay;
+      //   }
+
+      //   console.log("numero de hoy", todayNumber);
+      //   console.log("days in month", days);
 
       return (
         <table>
-          <tr>
-            <th>Traditional {shipping} freight</th>
-            <th>
-              {startCountry} - {destinationCountry}
-            </th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Traditional {shipping} freight</th>
+              <th>
+                {startCountry} - {destinationCountry}
+              </th>
+            </tr>
+          </thead>
           <tbody>
             <tr>
-              <td>
-                {startDay} - {endDay} days
-              </td>
+              <td>{startDay}</td>
               <td>US $</td>
             </tr>
             <tr>
-              <td>Estimated delivery</td>
+              <td>
+                {/* {monthName} {deliveryStart} - {monthName} {deliveryEnd} */}
+              </td>
               <td>info {quotePrice}</td>
             </tr>
           </tbody>
@@ -84,8 +116,6 @@ const Form = () => {
     }
     return null;
   };
-
-  console.log(inputs);
 
   return (
     <div>
